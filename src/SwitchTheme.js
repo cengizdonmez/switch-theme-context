ff4f2e2bac3910bfa2510b2fe09b8bde7eb39b45
Cengiz Dönmez <c.donmezzz@gmail.com>
2022-09-04 15:24:10 +0300

    Switch theme button svg icon added

--- src/SwitchTheme.js
+++ src/SwitchTheme.js
@@ -1,4 +1,6 @@
 import { useSite } from "./context/SiteContext";
+import SunIcon from "./assets/sun.svg";
+import MoonIcon from "./assets/moon.svg";
 
 export default function SwitchTheme() {
   const { theme, setTheme } = useSite();
@@ -9,7 +11,13 @@ export default function SwitchTheme() {
         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         type="button"
         class="mx-auto inline-flex items-center rounded-full border border-transparent bg-indigo-600 p-3 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-        <span className="text-lg">{theme === "dark" ? "light" : "dark"}</span>
+        <span>
+          {theme === "dark" ? (
+            <img className="h-10 w-10" src={SunIcon} alt="" />
+          ) : (
+            <img className="h-10 w-10" src={MoonIcon} alt="" />
+          )}
+        </span>
       </button>
     </>
   );
